df7d9c2175a4f37d91dd5de62fff0d4b88615ff5
Wokiput <71272739+Wokiput@users.noreply.github.com>
2020-10-15 19:51:13 +0700

    Add files via upload

--- cod/script.js
+++ cod/script.js
@@ -1,16 +1,5 @@
 window.onload = init;
 var formula = { "ch4" : "Метан", "c2h6" : "Этан", "c3h8" : "Пропан", "c4h10" : "Бутан", "c5h12" : "Пентан", "c6h14" : "Гексан", "c7h16" : "Гептан", "c8h18" : "Октна", "c9h20" : "Нонан", "c10h22" : "Декан"};
-var formulaimg = {
-"ch4" : "https://github.com/Wokiput/Site/blob/Test/img/ch4.png", 
-"c2h6" : "https://github.com/Wokiput/Site/blob/Test/img/c2h6.png", 
-"c3h8" : "https://github.com/Wokiput/Site/blob/Test/img/c3h8.png", 
-"c4h10" : "https://github.com/Wokiput/Site/blob/Test/img/c4h10.png", 
-"c5h12" : "https://github.com/Wokiput/Site/blob/Test/img/c5h12.png", 
-"c6h14" : "https://github.com/Wokiput/Site/blob/Test/img/c6h14.png", 
-"c7h16" : "https://github.com/Wokiput/Site/blob/Test/img/c7h16.png", 
-"c8h18" : "https://github.com/Wokiput/Site/blob/Test/img/c8h18.png", 
-"c9h20" : "https://github.com/Wokiput/Site/blob/Test/img/c9h20.png", 
-"c10h22" : "https://github.com/Wokiput/Site/blob/Test/img/c10h22.png"};
 function init(){
     var button = document.getElementById("addButton")
     button.onclick = handleButtonClick;
@@ -22,4 +11,5 @@ function init(){
       document.getElementById('block1').innerHTML = formula[stroka];
     }
     else document.getElementById('block1').innerHTML = "Что-то пошло не так";
+    document.getElementById("image").src = 'https://github.com/Wokiput/Site/tree/Test/img'+stroka.toLowerCase()+'.png'
   }
